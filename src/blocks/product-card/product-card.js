fb217e9e92d9edaaf33b3ae636cbe33db67e6ebd
AndrusovaK <andrusova_ksenia@mail.ru>
2017-01-14 14:12:02 +0300

    min/max orderNumbers in json and product card calculation

--- src/blocks/product-card/product-card.js
+++ src/blocks/product-card/product-card.js
@@ -2,8 +2,9 @@
 	$(function () {
 		$(document).on('click', '.number__minus', function () {
 			var $input = $(this).closest('.product-card__number').find('input');
+			var minNumber = Number($(this).closest('.product-card').attr('data-min-number')) || 1;
 			var count = parseInt($input.val()) - 1;
-			count = count < 1 ? 1 : count;
+			count = count < minNumber ? minNumber : count;
 			$input.val(count);
 			$input.change();
 			return false;
@@ -11,7 +12,10 @@
 
 		$(document).on('click', '.number__plus', function () {
 			var $input = $(this).closest('.product-card__number').find('input');
-			$input.val(parseInt($input.val()) + 1);
+			var maxNumber = Number($(this).closest('.product-card').attr('data-max-number')) || 10000;
+			var count = parseInt($input.val()) + 1;
+			count = count > maxNumber ? maxNumber : count;
+			$input.val(count);
 			$input.change();
 			return false;
 		});
@@ -19,9 +23,24 @@
 		var calcProductPrice = function () {
 			var numberOfBalloons = Number($(this).val()),
 				currentProduct = $(this).closest('.product-card'),
-				currentProductPrice = Number(currentProduct.attr('data-price'));
+				currentProductPrice = Number(currentProduct.attr('data-price')),
+				minOrderNumber = Number(currentProduct.attr('data-min-number')),
+				maxOrderNumber = Number(currentProduct.attr('data-max-number'));
+
+			if (numberOfBalloons < minOrderNumber) {
+				numberOfBalloons = minOrderNumber;
+				$(this).val(minOrderNumber);
+			} else if (numberOfBalloons > maxOrderNumber) {
+				numberOfBalloons = maxOrderNumber;
+				$(this).val(maxOrderNumber);
+			}
+
 			currentProduct.find('.product-card__price').html(numberOfBalloons*currentProductPrice + ' руб');
 		};
+
+		$('.product-card .number__input').each(function (ind, elem) {
+			calcProductPrice.call(elem);
+		});
 
 		$(document).on('change', '.product-card .number__input', calcProductPrice);
 
